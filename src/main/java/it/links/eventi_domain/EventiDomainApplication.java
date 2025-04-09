fc55625079a6af65dc3b15b897f257656c915c26
AngRafFrancavilla <francavillaangeloraffaele@gmail.com>
2025-04-09 10:12:50 +0200

    Importo progetto su github

---
+++ src/main/java/it/links/eventi_domain/EventiDomainApplication.java
@@ -0,0 +1,13 @@
+package it.links.eventi_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventiDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventiDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/links/eventi_domain/EventiDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package it.links.eventi_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventiDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
